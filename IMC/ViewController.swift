3048f4b65fa2ba8ea5f264c56d48c9bd73128219
Adriancys Jesus Villegas Toro <81894293+adriancysvillegast@users.noreply.github.com>
2021-10-14 10:00:44 -0400

    Initial Commit

---
+++ IMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Adriancys Jesus Villegas Toro on 14/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
