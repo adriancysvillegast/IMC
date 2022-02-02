d428cff4b44cf24576118bb83b08c605cf4a0218
Adriancys Jesus Villegas Toro <81894293+adriancysvillegast@users.noreply.github.com>
2022-02-01 23:15:08 -0400

    first commit

---
+++ IMC/Model/BMI.swift
@@ -0,0 +1,8 @@
+//
+//  BMI.swift
+//  IMC
+//
+//  Created by Adriancys Jesus Villegas Toro on 19/10/21.
+//
+
+import Foundation
---
+++ IMC/Model/CalculateBrain.swift
@@ -0,0 +1,8 @@
+//
+//  CalculateBrain.swift
+//  IMC
+//
+//  Created by Adriancys Jesus Villegas Toro on 19/10/21.
+//
+
+import Foundation
